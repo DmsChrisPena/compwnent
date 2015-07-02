adf6b0e895ab0b9a3a8480b901a7ace90a4dc37f
Chris N Pena <Cpena1@Chriss-MacBook-Pro.local>
2015-07-02 11:41:23 -0500

    Pushing all new changes to matthew

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
-var app = angular.module("compwnentApp", ['firebase', 'ngRoute']);
+angular.module('filters', []).
+    filter('truncate', function () {
+        return function (text, length, end) {
+            if (isNaN(length))
+                length = 10;
+
+            if (end === undefined)
+                end = "...";
+
+            if (text.length <= length || text.length - end.length <= length) {
+                return text;
+            }
+            else {
+                return String(text).substring(0, length-end.length) + end;
+            }
+
+        };
+    });
+
+
+var app = angular.module("compwnentApp", ['firebase', 'ngRoute', 'filters']);
 
 	app.controller("compwnentCtrl", ['$scope', '$firebaseArray', function($scope, $firebaseArray){
 		var ref = new Firebase("https://compwnents.firebaseio.com/");
